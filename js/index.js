bf7dab2d446ca004a9fe4bf76bd7e2e602222d06
Alegar565 <69178710+Alegar565@users.noreply.github.com>
2023-06-22 21:42:25 -0400

    subida1

---
+++ js/index.js
@@ -0,0 +1,52 @@
+let formActions = {
+    save: function (file, directory) {
+        let form = document.querySelector('#text-form');
+        form.action = "./guardar.php?archivo=" + file + "&directorio=" + directory;
+        form.submit();
+    },
+    saveAs: function (file, directory) {
+        let form = document.querySelector('#text-form');
+        form.action = "./explorador.php?guardar-como&archivo=" + file + "&directorio=" + directory;
+        form.submit();
+    }
+}
+
+let actions = {
+    action: function (action, file, directory) {
+        switch (action) {
+            case "Abrir":
+                window.location.href = "./?archivo=" + file + "&directorio=" + directory;
+                break;
+
+            case "Reemplazar":
+                let form = document.querySelector('#text-form');
+                form.action = "./guardar.php?archivo=" + file + "&directorio=" + directory;
+                form.submit();
+                break;
+        }
+    },
+    delete: function (file, directory) {
+        window.location.href = "./eliminar.php?archivo=" + file + "&directorio=" + directory;
+    },
+    accept: function (action, file, directory) {
+        switch (action) {
+            case "Abrir":
+                window.location.href = "./?archivo=" + file + "&directorio=" + directory;
+                break;
+
+            case "Reemplazar":
+                let form = document.querySelector('#text-form');
+
+                form.action = "./guardar.php?archivo=" + file + "&directorio=" + directory;
+                form.submit();
+                break;
+        }
+    },
+    cancel: function () {
+        window.location.href = "./";
+    },
+    createFolder: function (file, directory) {
+        window.location.href = "./nueva-carpeta.php?archivo=" + file + "&directorio=" + directory;
+    }
+    
+}
